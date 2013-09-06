0b5a44151829477ca74999fc5bb7bbf0763fb1af
Andrew Young <ayoung@kabam.com>
2013-09-05 17:07:49 -0700

    Added arrow key navigation and commands to easily start and stop the emacs server.

---
+++ personal/server-shutdown.el
@@ -0,0 +1,6 @@
+;; define function to shutdown emacs server instance
+(defun server-shutdown ()
+  "Save buffers, Quit, and Shutdown (kill) server"
+  (interactive)
+  (save-some-buffers)
+  (kill-emacs))
