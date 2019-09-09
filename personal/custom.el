376ab35997a10d199fc682003ccbdaef8c700b4d
Andrew Young <andrew@istellar.jp>
2019-09-09 14:11:05 +0900

    ERC updates

--- personal/custom.el
+++ personal/custom.el
@@ -3,6 +3,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(erc-modules
+   (quote
+    (autojoin button completion fill irccontrols list match menu move-to-prompt netsplit networks noncommands notifications readonly ring stamp track)))
+ '(erc-nick "AndrewYoung")
+ '(erc-user-full-name "Andrew Young")
  '(package-selected-packages
    (quote
     (helm-ag json-mode golint sly-repl-ansi-color sly-asdf easy-hugo markdown-mode rainbow-delimiters helm exec-path-from-shell zop-to-char zenburn-theme which-key volatile-highlights undo-tree super-save smartrep smartparens operate-on-number move-text magit projectile imenu-anywhere hl-todo guru-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region epl editorconfig easy-kill diminish diff-hl discover-my-major crux browse-kill-ring beacon anzu ace-window))))
