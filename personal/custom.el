8d0206287360d4fc723bb1c40f4b4388ea97da96
Andrew Young <andrew@istellar.jp>
2020-01-15 18:02:01 +0900

    Updated custom.el

--- personal/custom.el
+++ personal/custom.el
@@ -20,11 +20,13 @@
  '(package-enable-at-startup t)
  '(package-selected-packages
    (quote
-    (dockerfile-mode protobuf-mode yaml-mode mips-mode go-autocomplete go-eldoc fill-column-indicator go-mode ac-sly company elpy erc-crypt helm-ag json-mode golint sly-repl-ansi-color sly-asdf easy-hugo markdown-mode rainbow-delimiters helm exec-path-from-shell zop-to-char zenburn-theme which-key volatile-highlights undo-tree super-save smartrep smartparens operate-on-number move-text magit projectile imenu-anywhere hl-todo guru-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region epl editorconfig easy-kill diminish diff-hl discover-my-major crux browse-kill-ring beacon anzu ace-window)))
+    (gnu-elpa-keyring-update cobol-mode gradle-mode dockerfile-mode protobuf-mode yaml-mode mips-mode go-autocomplete go-eldoc fill-column-indicator go-mode ac-sly company elpy erc-crypt helm-ag json-mode golint sly-repl-ansi-color sly-asdf easy-hugo markdown-mode rainbow-delimiters helm exec-path-from-shell zop-to-char zenburn-theme which-key volatile-highlights undo-tree super-save smartrep smartparens operate-on-number move-text magit projectile imenu-anywhere hl-todo guru-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region epl editorconfig easy-kill diminish diff-hl discover-my-major crux browse-kill-ring beacon anzu ace-window)))
  '(python-shell-interpreter "python3"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(column-marker-1 ((t (:background "gray20"))))
+ '(column-marker-2 ((t (:background "gray20"))))
+ '(column-marker-3 ((t (:background "gray30")))))
